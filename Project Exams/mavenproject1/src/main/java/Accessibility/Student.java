4e96d579502aba0e8896b7fb2a3577b6a22d1554
Omar2412O <84752474+Omar2412O@users.noreply.github.com>
2022-04-29 13:59:30 +0200

    Student and principle class constructor done and student functions declarations

--- Project Exams/mavenproject1/src/main/java/Accessibility/Student.java
+++ Project Exams/mavenproject1/src/main/java/Accessibility/Student.java
@@ -1,18 +1,45 @@
 
 package Accessibility;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Student extends User{
     
     private int grade;
-    private String issue_report;
-    private String note_to_principle;
+    private ArrayList<String> subjects;
+    
+    private HashMap<String,Report> issueReport;
+    private String noteToPrinciple;
+    private ArrayList<Exam> allExams;
+    private int enrolledclassID;
 
+    public Student(ArrayList<String> subjects, int enrolledclassID, String name, String username, String password, String role) {
+        super(name, username, password, role);
+        this.subjects = subjects;
+        this.enrolledclassID = enrolledclassID;
+    }
+
+    
+    public void CalculateFinalGrade(){
+        
+    }
+    public void ShowExamGrades(){
+        
+    }
+    public void TakeExam(){
+        
+    }
+    public void ShowReport(){
+        
+    }
+   
     public String getNote_to_principle() {
-        return note_to_principle;
+        return noteToPrinciple;
     }
 
     public void setNote_to_principle(String note_to_principle) {
-        this.note_to_principle = note_to_principle;
+        this.noteToPrinciple = note_to_principle;
     }
 
     public void setGrade(int grade) {
@@ -23,12 +50,12 @@ public class Student extends User{
         return grade;
     }
 
-    public String getIssue_report() {
-        return issue_report;
+    public HashMap<String, Report> getIssue_report() {
+        return issueReport;
     }
 
-    public void setIssue_report(String issue_report) {
-        this.issue_report = issue_report;
+    public void setIssue_report(HashMap<String,Report> issue_report) {
+        this.issueReport = issue_report;
     }
     
     public void ViewGrades(){
