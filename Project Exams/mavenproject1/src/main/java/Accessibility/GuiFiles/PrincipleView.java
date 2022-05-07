6595bba0799c9242faf21d1d6a38899608b7e383
seifhossam33 <84685631+seifhossam33@users.noreply.github.com>
2022-05-08 00:10:51 +0200

    Continue the Set Scheduling

--- Project Exams/mavenproject1/src/main/java/Accessibility/GuiFiles/PrincipleView.java
+++ Project Exams/mavenproject1/src/main/java/Accessibility/GuiFiles/PrincipleView.java
@@ -63,6 +63,11 @@ public class PrincipleView extends javax.swing.JFrame {
 
         jButton4.setFont(new java.awt.Font("Dialog", 3, 18)); // NOI18N
         jButton4.setText("Set Schedule");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
 
         jButton5.setFont(new java.awt.Font("Dialog", 3, 18)); // NOI18N
         jButton5.setText("View Teachers' Details");
@@ -137,6 +142,12 @@ public class PrincipleView extends javax.swing.JFrame {
         new ViewTeacherDetails().setVisible(true);
         this.setVisible(false);
     }//GEN-LAST:event_jButton5ActionPerformed
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        // TODO add your handling code here:
+        new SetSchedule().setVisible(true);
+        this.setVisible(false);
+    }//GEN-LAST:event_jButton4ActionPerformed
 
     /**
      * @param args the command line arguments
